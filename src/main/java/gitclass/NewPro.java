d41eeb83f5287848ef27f1a84877ad721b77a268
jayaabhi <jayalakshmi.a1996@gmail.com>
2022-09-02 12:30:35 +0530

    FirstCommit

---
+++ src/main/java/gitclass/NewPro.java
@@ -0,0 +1,18 @@
+package gitclass;
+
+import org.testng.annotations.Test;
+
+public class NewPro {
+	@Test
+	public void test1() {
+		System.out.println("test1");
+		
+	}
+	@Test
+	public void test2() {
+		System.out.println("test2");
+	}
+
+		
+
+}
